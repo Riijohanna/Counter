c16afb87610ab251dc9641853f9ce178d5cb62f5
Riijohanna <riikka.jharju@gmail.com>
2019-10-20 22:01:10 +0300

    with redux

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import Counter from "./Counter.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p></p>
+      </header>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
